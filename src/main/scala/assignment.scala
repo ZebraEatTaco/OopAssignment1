e4b2be4f6ead445efc1a23ab9ae533addb2279da
kyevin <20010773@imail.sunway.edu.my>
2024-06-03 22:02:59 +0800

    v2

--- src/main/scala/assignment.scala
+++ src/main/scala/assignment.scala
@@ -1,13 +1,25 @@
-//https://www.freeprojectz.com/paid-projects/nodejs-react-mongo/online-food-ordering-system
+case class Login(username: String, password: String)
 
-case class Login(username:String, password:String)
+abstract class User(val name: String, val email: String, val gender: String, val phoneNumber: String, val address: String)
 
-abstract class User(val name:String, val email:String, val gender:String, val phoneNumber: Int, val address:String)
+class Customer(_name: String, _email: String, _gender: String, _phoneNumber: String, _address: String) extends User(_name, _email, _gender, _phoneNumber, _address)
 
-class Customer(_name:String,_email:String,_gender:String,_phoneNumber:Int,_address:String) extends User(_name,_email,_gender,_phoneNumber,_address)
+class Admin(_name: String, _email: String, _gender: String, _phoneNumber: String, _address: String) extends User(_name, _email, _gender, _phoneNumber, _address)
 
-class Admin(_name:String,_email:String,_gender:String,_phoneNumber:Int,_address:String) extends User(_name,_email,_gender,_phoneNumber,_address)
+case class FoodCategory(category: String)
 
-case class FoodCategory(Category: String)
+class Food(foodCategory: FoodCategory, val name: String, val price: Double, val company: String) {
+  var reviews: List[(Int, String)] = Nil
+  var ordersReceived: Int = 0
 
-class Food(var foodCategory: FoodCategory)
+  def addReview(rating: Int, comment: String): Unit = {
+    reviews = (rating, comment) :: reviews
+  }
+  def incrementOrdersReceived(): Unit = {
+    ordersReceived += 1
+  }
+
+
+}
+
+case class Order(id: Int, customer: Customer, company: String, food: Food, quantity: Int, totalPrice: Double)
