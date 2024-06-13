74c287a4668e2b56a0890a4b45692b08e00471ea
kyevin <20010773@imail.sunway.edu.my>
2024-06-13 16:18:02 +0800

    v3

--- src/main/scala/assignment.scala
+++ src/main/scala/assignment.scala
@@ -1,27 +1,48 @@
-case class Login(username: String, password: String)
-  def status(username, password):Boolean 
+case class Login(username: String, password: String) {
+  def status(_username: String, _password: String): Boolean = {
+    _username == username && _password == password
+  }
+}
 
-abstract class User(login:Login, val displayName: String, var email: String, val gender: String, var phoneNumber: String, var address: String)
-class Customer(_name: String, _email: String, _gender: String, _phoneNumber: String, _address: String) extends User(_name, _email, _gender, _phoneNumber, _address)
+abstract class User(val login: Login, val displayName: String, var email: String, val gender: String, var phoneNumber: String, var address: String)
 
-class Admin(_name: String, _email: String, _gender: String, _phoneNumber: String, _address: String) extends User(_name, _email, _gender, _phoneNumber, _address)
+class Customer(login: Login, displayName: String, email: String, gender: String, phoneNumber: String, address: String) extends User(login, displayName, email, gender, phoneNumber, address)
+
+class Admin(login: Login, displayName: String, email: String, gender: String, phoneNumber: String, address: String) extends User(login, displayName, email, gender, phoneNumber, address)
 
 case class Category(category: String)
 
-class Food(category: Category, val name: String, val price: Double, val company: String) {
+trait Discount {
+  val percentage: Double
+
+  def calculateDiscount(price: Double): Double = {
+    price * percentage / 100
+  }
+
+  def applyDiscount(price: Double): Double = {
+    price - calculateDiscount(price)
+  }
+}
+
+class Food(val category: Category, val name: String, val price: Double, val company: String) extends Discount {
   var reviews: List[(Int, String)] = Nil
   var ordersReceived: Int = 0
 
   def addReview(rating: Int, comment: String): Unit = {
     reviews = (rating, comment) :: reviews
   }
+
   def incrementOrdersReceived(): Unit = {
     ordersReceived += 1
   }
 
-
+  def finalPrice: Double = {
+    applyDiscount(price)
+  }
 }
 
-case class Order(id: Int, customer: Customer, company: String, food: Food, quantity: Int, totalPrice: Double)
-
-trait Discount()
+case class Order(id: Int, customer: Customer, company: String, food: Food, quantity: Int) {
+  def totalPrice: Double = {
+    food.finalPrice * quantity
+  }
+}
