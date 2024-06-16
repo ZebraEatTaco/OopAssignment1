ff18735fe38875fd4f314681097595daa7b6d00a
kyevin <20010773@imail.sunway.edu.my>
2024-06-16 17:44:03 +0800

    v4

--- src/main/scala/assignment.scala
+++ src/main/scala/assignment.scala
@@ -1,10 +1,52 @@
-abstract class User(val username: String, var displayName: String, var email: String, val gender: String, var phoneNumber: String, var address: String)
+abstract class User(val username: String, var displayName: String, var email: String, val gender: String, var phoneNumber: String, var address: String) {
+  def updateProfile(newDisplayName: String, newEmail: String, newPhoneNumber: String, newAddress: String): Unit = {
+    // Update user profile details
+  }
+
+  def resetPassword(oldPassword: String, newPassword: String): Boolean = {
+    // Reset the user's password and return success status
+    false
+  }
+
+  def deactivateAccount(): Unit = {
+    // Deactivate the user's account
+  }
+}
 
 class Customer(username: String, displayName: String, email: String, gender: String, phoneNumber: String, address: String, val login: Login)
-  extends User(username, displayName, email, gender, phoneNumber, address)
+  extends User(username, displayName, email, gender, phoneNumber, address) {
+
+  def viewOrderHistory(): List[Order] = {
+    // Return a list of orders placed by the customer
+    Nil
+  }
+
+  def placeOrder(order: Order): Boolean = {
+    // Place a new order and return success status
+    false
+  }
+
+  def addReview(food: Food, rating: Int, comment: String): Unit = {
+    // Add a review for the specified food item
+  }
+}
 
 class Admin(username: String, displayName: String, email: String, gender: String, phoneNumber: String, address: String, val login: Login)
-  extends User(username, displayName, email, gender, phoneNumber, address)
+  extends User(username, displayName, email, gender, phoneNumber, address) {
+
+  def manageUsers(): Unit = {
+    // Logic to manage users (add, remove, update)
+  }
+
+  def manageFoodItems(): Unit = {
+    // Logic to manage food items (add, remove, update)
+  }
+
+  def viewAllOrders(): List[Order] = {
+    // Return a list of all orders in the system
+    Nil
+  }
+}
 
 class DeliveryPerson(username: String, displayName: String, email: String, gender: String, phoneNumber: String, address: String, val login: Login)
   extends User(username, displayName, email, gender, phoneNumber, address) {
@@ -57,11 +99,11 @@ object User {
 case class Category(category: String, description:String)
 
 class Food(var category: Category, var name: String, var price: Double, val restaurant: Restaurant) {
-  var reviews: List[(Int, String)] = Nil
+  var reviews: List[(Customer, Int, String)] = Nil
   var ordersReceived: Int = 0
 
-  def addReview(rating: Int, comment: String): Unit = {
-    reviews = (rating, comment) :: reviews
+  def addReview(customer:Customer, rating: Int, comment: String): Unit = {
+    reviews = (customer, rating, comment) :: reviews
   }
 
   def incrementOrdersReceived(): Unit = {
@@ -71,7 +113,7 @@ class Food(var category: Category, var name: String, var price: Double, val rest
 }
 
 trait Discount {
-  val percentage: Double
+  var percentage: Double
 
   def calculateDiscount(price: Double): Double = {
     price * percentage / 100
